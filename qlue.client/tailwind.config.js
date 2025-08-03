2ffa57ade8e89c32d69c88f3bb83d71123a04932
manasseh-zw <manassehtchanga@gmail.com>
2025-08-03 02:02:45 +0200

    fix: apply site styles

--- qlue.client/tailwind.config.js
+++ qlue.client/tailwind.config.js
@@ -3,6 +3,7 @@ import { heroui } from "@heroui/react";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
--- qlue.client/vite.config.ts
+++ qlue.client/vite.config.ts
@@ -10,4 +10,7 @@ export default defineConfig({
       "@": resolve(__dirname, "./src"),
     },
   },
+  css: {
+    postcss: "./postcss.config.js",
+  },
 });
